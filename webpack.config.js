5a557f550a28b26377a8ed84de697432b88898a8
Mohit Chandarana <mchandarana@codio.com>
2025-06-27 19:03:38 +0000

    Checkpoint for beginning of Output management

--- webpack.config.js
+++ webpack.config.js
@@ -7,28 +7,4 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     // Add publicPath here
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ['csv-loader'],
-      },
-      {
-        test: /\.xml$/i,
-        use: ['xml-loader'],
-      },
-    ],
-  },
 };
