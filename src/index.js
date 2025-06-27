fc4cbfc239d1af5a4c0f78dfdaa5337b496ecc1c
Mohit Chandarana <mchandarana@codio.com>
2025-06-27 20:15:49 +0000

    End of demo

--- src/index.js
+++ src/index.js
@@ -1,39 +1,18 @@
-// // import printMe from './print.js';
-
-// async function getComponent() {
-//   const element = document.createElement('div');
-//   const { default: _ } = await import('lodash');
-
-//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-// //   const btn = document.createElement('button');
-
-  
-
-// //   btn.innerHTML = 'Click me and check the console!';
-// //   btn.onclick = printMe;
-
-// //   element.appendChild(btn);
-
-        
-//   return element;
-// }
-
-// getComponent().then((component) => {
-//   document.body.appendChild(component);
-// });
-
 import { cube } from './math.js';
 
-  function component() {
-   const element = document.createElement('pre');
+if (process.env.NODE_ENV !== 'production') {
+ console.log('Looks like we are in development mode!');
+}
 
-   element.innerHTML = [
-     'Hello webpack!',
-     '5 cubed is equal to ' + cube(5)
-   ].join('\n\n');
+function component() {
+ const element = document.createElement('pre');
 
-    return element;
-  }
+ element.innerHTML = [
+    'Hello webpack!',
+    '5 cubed is equal to ' + cube(5)
+ ].join('\n\n');
 
-  document.body.appendChild(component());
+ return element;
+}
 
+document.body.appendChild(component());
